fe5f6e09b0c04fc41c32bd2a7e7406f39cd545b8
violetyy0305 <137112254+violetyy0305@users.noreply.github.com>
2023-08-26 23:08:56 +0000

    added profile and store data to db

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
 from flask import Flask, render_template, jsonify, request
+from database import load_profile_questions_from_db, add_survey_to_db
 
-app=Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route("/")
 def hello_mind():
-  survey=load_survey_from_db()
+  profile_questions = load_profile_questions_from_db()
+  return render_template("survey.html", profile_questions=profile_questions)
+
+
+@app.route("/survey/submit", methods=['post'])
+def submit_survey():
+  data = request.form
+  profile_questions=load_profile_questions_from_db()
+  add_survey_to_db(data)
+
+  return render_template("survey_submitted.html",
+                    profile_answers=data,
+                    profile_questions=profile_questions)
+ 
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,4 +1,58 @@
 from sqlalchemy import create_engine, text
+import os
 
-db_connection_string=""
-engine=create_engine()
+db_connection_string=os.environ['DB_CONNECTION_STRING']
+
+engine=create_engine(db_connection_string,
+                    connect_args=
+                     {
+                      "ssl":{
+                        "ssl_ca": "/etc/ssl/cert.pem"
+                      }
+                    })
+
+def load_profile_questions_from_db():
+  with engine.connect() as conn:
+    result=conn.execute(text("select * from profile_questions"))
+    profile_questions=[]
+    keys=list(result.keys())
+    for question in result.all():
+      d={}
+      for i in range(len(question)):
+        d[keys[i]]=question[i]
+      profile_questions.append(d)
+
+  return profile_questions
+
+
+def add_survey_to_db(data):
+  with engine.connect() as conn:
+    query=text("insert into profile_answers(gender,age_group,employment) values(:gender,:age_group, :employment)")
+    conn.execute(query,
+                {
+                 "gender":data['gender'],
+                  "employment":data['employment'],
+                  "age_group":data['age_group']})
+
+
+
+
+
+
+
+
+
+def load_profile_question_from_db(id):
+  with engine.connect() as conn:
+    result=conn.execute(text("select * from profile_questions where id=:val"),{"val":id})
+    row=result.all()
+    if len(row)==0:
+      return None
+    else:
+      key=list(result.keys())
+      d={}
+      for i in range(len(row[0])):
+        d[key[i]]=row[0][i]
+      return d
+
+
