795b76eba509ace5db7c0c3d6e9ac7c3b6742f90
violetyy0305 <137112254+violetyy0305@users.noreply.github.com>
2023-08-27 05:31:29 +0000

    added survey

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, jsonify, request
-from database import load_profile_questions_from_db, add_survey_to_db
+from database import load_profile_questions_from_db, load_survey_questions_from_db, add_profile_to_db, add_survey_to_db
+
+from generate_radom_survey import generate_random_survey
 
 app = Flask(__name__)
 
@@ -7,19 +9,25 @@ app = Flask(__name__)
 @app.route("/")
 def hello_mind():
   profile_questions = load_profile_questions_from_db()
-  return render_template("survey.html", profile_questions=profile_questions)
+  survey_questions=generate_random_survey()
+  return render_template("survey.html", profile_questions=profile_questions,
+                        survey_questions=survey_questions)
 
 
 @app.route("/survey/submit", methods=['post'])
 def submit_survey():
   data = request.form
   profile_questions=load_profile_questions_from_db()
+  add_profile_to_db(data)
+
+  survey_questions=generate_random_survey()
   add_survey_to_db(data)
 
   return render_template("survey_submitted.html",
                     profile_answers=data,
-                    profile_questions=profile_questions)
- 
+                    profile_questions=profile_questions,
+                    survey_questions=survey_questions)
+
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -1,58 +1,68 @@
 from sqlalchemy import create_engine, text
 import os
 
-db_connection_string=os.environ['DB_CONNECTION_STRING']
+db_connection_string = os.environ['DB_CONNECTION_STRING']
 
-engine=create_engine(db_connection_string,
-                    connect_args=
-                     {
-                      "ssl":{
-                        "ssl_ca": "/etc/ssl/cert.pem"
-                      }
-                    })
+engine = create_engine(db_connection_string,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
+
 
 def load_profile_questions_from_db():
   with engine.connect() as conn:
-    result=conn.execute(text("select * from profile_questions"))
-    profile_questions=[]
-    keys=list(result.keys())
+    result = conn.execute(text("select * from profile_questions"))
+    profile_questions = []
+    keys = list(result.keys())
     for question in result.all():
-      d={}
+      d = {}
       for i in range(len(question)):
-        d[keys[i]]=question[i]
+        d[keys[i]] = question[i]
       profile_questions.append(d)
 
   return profile_questions
 
 
+def add_profile_to_db(data):
+  with engine.connect() as conn:
+    query = text(
+      "insert into profile_answers(gender,age_group,employment) values(:gender,:age_group, :employment)"
+    )
+    conn.execute(
+      query, {
+        "gender": data['gender'],
+        "employment": data['employment'],
+        "age_group": data['age_group']
+      })
+
+
+def load_survey_questions_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from survey_questions"))
+    survey_questions = []
+    keys = list(result.keys())
+    for question in result.all():
+      d = {}
+      for i in range(len(question)):
+        d[keys[i]] = question[i]
+      survey_questions.append(d)
+
+  return survey_questions
+
+
 def add_survey_to_db(data):
   with engine.connect() as conn:
-    query=text("insert into profile_answers(gender,age_group,employment) values(:gender,:age_group, :employment)")
-    conn.execute(query,
-                {
-                 "gender":data['gender'],
-                  "employment":data['employment'],
-                  "age_group":data['age_group']})
-
-
-
-
-
-
-
-
-
-def load_profile_question_from_db(id):
-  with engine.connect() as conn:
-    result=conn.execute(text("select * from profile_questions where id=:val"),{"val":id})
-    row=result.all()
-    if len(row)==0:
-      return None
-    else:
-      key=list(result.keys())
-      d={}
-      for i in range(len(row[0])):
-        d[key[i]]=row[0][i]
-      return d
-
-
+    query = text(
+      "insert into survey_answers(question_1,question_2,question_3,question_4,question_5,question_6,question_7,question_8) values(:question_1,:question_2,:question_3,:question_4,:question_5,:question_6,:question_7,:question_8)"
+    )
+    conn.execute(
+      query, {
+        "question_1": data['question_1'],
+        "question_2": data['question_2'],
+        "question_3": data['question_3'],
+        "question_4": data['question_4'],
+        "question_5": data['question_5'],
+        "question_6": data['question_6'],
+        "question_7": data['question_7'],
+        "question_8": data['question_8']
+      })
